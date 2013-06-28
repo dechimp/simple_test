598d8d1e437acb56a255205b94a564d6506d7d50
Richard Manriquez <dechimp@gmail.com>
2013-06-27 22:01:27 -0700

    Add working RSpec / Capybara Log In tests with YAML test data

---
+++ spec/log_in.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "can I log in?", :type => :feature do
+  it "doesn't log in with bad credentials", :js => true do
+    #init user data
+    users = YAML.load_file('factories/users.yaml')
+    user = users["tom"]
+    username = user[0]["username"]
+    password = user[1]["password"]
+    expect(username).to eq("tomservo")
+    expect(password).to eq("pumaMan2")
+
+    #visit site and log in
+    visit 'https://www.simple.com/signin'
+    current_url.should == 'https://www.simple.com/signin'
+    within("#signin") do
+      fill_in 'login_username', :with => username
+      fill_in 'login_password', :with => password
+    end
+    click_button 'Sign in...'
+    expect(page).to have_content 'Your username and passphrase don\'t match'
+  end
+
+  #it "logs in with good credentials", :js => true do
+    #visit 'https://www.simple.com/signin'
+    #within("#signin") do
+      #fill_in 'login_username', :with => 'tomservo'
+      #fill_in 'login_password', :with => 'pumaMan2'
+    #end
+    #click_button 'Sign in...'
+    #expect(page).to have_content 'Richard Manriquez'
+  #end
+end
